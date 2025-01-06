044d1fac257b828748cd079a2e005ff067d586a0
unknown <avivheldman@gmail.com>
2025-01-06 14:12:25 +0700

    added base files

---
+++ src/test/Topic.java
@@ -0,0 +1,24 @@
+package test;
+
+public class Topic {
+    public final String name;
+    Topic(String name){
+        this.name=name;
+    }
+
+    public void subscribe(Agent a){
+    }
+    public void unsubscribe(Agent a){
+    }
+
+    public void publish(Message m){
+    }
+
+    public void addPublisher(Agent a){
+    }
+
+    public void removePublisher(Agent a){
+    }
+
+
+}
